ab1068a00044ff294756020b443b5e9e483184fa
pengxinjie <1370639979@qq.com>
2020-05-29 19:54:30 +0800

    ATM模拟系统2.0 修复存取款异常

--- src/ATM.java
+++ src/ATM.java
@@ -1,19 +1,32 @@
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  * @author Pengxinjie
  * @DATE 2020/5/29 - 14:52
  */
 public class ATM {
-
+    /**
+     * 账户名称
+     */
     private Account acc;
-
+    /**
+     * 储存数据的文件
+     */
     private File dataFile;
+    /**
+     * 文件写入
+     */
     private FileWriter fw;
     private BufferedWriter bw;
-
+    /**
+     * 文件路径
+     */
     private String filePath = "./data.txt";
 
+    /**调用此构造器将新建一个账户
+     *
+     */
     public ATM() {
         this.acc = new Account();
         try {
@@ -31,6 +44,11 @@ public class ATM {
         }
     }
 
+    /**
+     *
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         new ATM().interact();
     }
@@ -39,12 +57,12 @@ public class ATM {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("账号:");
-        String Account = br.readLine();
+        String AccountName = br.readLine();
         String password;
         while (true) {
             System.out.print("密码：");
             password = br.readLine();
-            if (!this.acc.isValid(Long.parseLong(Account.trim()), password.trim())) {
+            if (!this.acc.isValid(Long.parseLong(AccountName.trim()), password.trim())) {
                 System.err.println("密码错误!请重新输入！");
                 System.out.println();
             }else{
@@ -75,10 +93,10 @@ public class ATM {
                     break;
                 case 2:
                     System.out.print("取款数额：");
-                    String temp3 = br.readLine();
+                    String Num = br.readLine();
                     try {
-                        long ammount = Long.parseLong(password.trim());
-                        this.acc.withdraw(ammount);
+                        long Amm = Long.parseLong(Num.trim());
+                        this.acc.withdraw(Amm);
                         break;
                     } catch (NumberFormatException nfe) {
                         System.err.println("无效的数字！");
@@ -86,10 +104,10 @@ public class ATM {
                     }
                 case 3:
                     System.out.print("存款数额：");
-                    String temp4 = br.readLine();
+                    Num = br.readLine();
                     try {
-                        long ammount = Long.parseLong(password.trim());
-                        this.acc.deposit(ammount);
+                        long Amm = Long.parseLong(Num.trim());
+                        this.acc.deposit(Amm);
                         break;
                     } catch (NumberFormatException nfe) {
                         System.err.print("无效的数字！");
@@ -97,24 +115,24 @@ public class ATM {
                     }
                 case 4:
                     System.out.print("旧密码：");
-                    String temp5 = br.readLine();
+                    String oldPassword = br.readLine();
                     System.out.print("新密码：");
-                    String temp6 = br.readLine();
-                    this.acc.changePassword(Account, password);
+                    String newPassword = br.readLine();
+                    this.acc.changePassword(oldPassword,newPassword);
                     break;
                 case 5:
                     this.bw.write(this.acc.toString());
                     break;
                 default:
+                    System.out.println("无效的输入！");
                     break;
             }
         }
     }
-
 }
 
-class Account {
 
+class Account {
     private long accNo = 123456;
     private String pass = "123456";
     private long balance = 10000;
@@ -133,11 +151,13 @@ class Account {
             return;
         }
         if (password.length() < 6) {
-            System.err.println("密码小于6位数！");
+            System.err.println("新密码不能小于6位数！");
+            System.out.println();
             return;
         }
         if (password.equals(this.pass)) {
             System.err.println("新密码不能与原密码重复！");
+            System.out.println();
             return;
         }
         this.pass = password;
@@ -149,11 +169,11 @@ class Account {
 
     public void withdraw(long amount) {
         if (amount > 5000 || amount < 0) {
-            System.err.println("数额范围: $0-$5000");
+            System.err.println("取款数额范围: $0-$5000");
             return;
         }
         if ((amount % 100) != 0) {
-            System.err.println("这个数额不是100的整数倍数！");
+            System.err.println("此数额不是100的整数倍数！");
             return;
         }
         long newBalance = this.balance - amount;
@@ -166,7 +186,7 @@ class Account {
 
     public void deposit(long amount) {
         if (amount < 0) {
-            System.err.println("数额不能为负数！");
+             System.err.println("数额不能为负数！");
             return;
         }
         this.balance += amount;
